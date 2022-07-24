816a76d068ed9bfb545d3ec5f2674a99793d09a6
tnfo222 <tayforbes92@hotmail.com>
2022-07-24 15:56:01 -0400

    Questions answered in command doc

--- commands.js
+++ commands.js
@@ -94,16 +94,26 @@
 
 
 // 3. Query for all bounties, but exclude the client attribute from being shown
-
+//    db.bounties.find({}, {client: 0})
 
 
 // 4. Query for a Groundhog in the Woodlands
+//    db.bounties.find({species: "Groundhog", location: "Woodlands"})
+//    db.bounties.find({&and: [{species: "Groundhog"}, {location: "Woodlands"}]})
 
 // Update and Delete
 // 1. Update the reward for Polarwind to 10000
+//    db.bounties.updateOne({name: "Polarwind"}, {$set: {reward: 10000}})
+
 
 // 2. Remove Lokinkajou
+//    db.bounties.findOneAndDelete({name: "Lokinkajou"})
+
+
 
 // 3. Delete all bounties sent by Songbird
+//   db.bounties.deleteMany({client: "Songbird"})
+
 
 // 4. Update all captured statuses to true
+//    db.bounties.updateMany({}, {$set: {captured: true}})
